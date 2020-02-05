c782f6a2e5e480ddf194e23cf9e0e9093ee71e91
ricardozv <ricardosilvanet03@gmail.com>
2020-02-05 17:37:39 -0500

    upload app.js

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const twilio = require ("twilio");
-var client = new twilio.RestClient("AC878ee57a7037b15bcf23aaf6b6df770d", "1eb70a6d5594ba9e76d1adefa97ba3c0");
-client.sms.messages.creat({
-    to: "+559881817051",
-    from:"+559884047409",
-    body:""
-}, function(error, message){
-    if(!error){
-        console.log("Mensagem enviada");
-    }else{
-        console.log("Mensagem não enviada")
-    }
+const twilio = require("twilio");
+const accountSid = 'AC878ee57a7037b15bcf23aaf6b6df770d';
+const authToken = '1eb70a6d5594ba9e76d1adefa97ba3c0';
+const client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+     body: 'Olá! Sou o romulo fernandes!',
+     from: '+18455934459',
+     to: '+5598981817051'
+   }, function(error, message) {
+       if(!error){
+           console.log("Mensagem enviada!")
+       }else{
+           console.log("Mensagem não enviada")
+       }
+   
 });
-
-
-
 
 
 
